cd98fcad69a051b2f7a8619600c4f059366f7268
liz <97650879+Spergliz@users.noreply.github.com>
2023-01-05 12:44:19 -0700

    hdr

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 let inputE1 = document.getElementById("input1");
 let inputE2 = document.getElementById("input2"); 
 let outputE1 = document.getElementById("tip-out");
+
+document.getElementById("calc").addEventListener("click", mathlol)
+function mathlol (){
+    let A = inputE1.value*inputE2.value
+    let B = Math.ceil(A)
+    outputE1.innerHTML= B
+}
